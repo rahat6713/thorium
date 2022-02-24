37950be2040f3f2d877b2fb3425a73e7c7fa638b
rahat6713 <rahatali6713@gmail.com>
2022-02-24 21:11:34 +0530

    Today's assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -42,41 +42,47 @@ router.post("/post-query-2", function (req, res) {
     // }
     res.send({ result: finalArr , status: true })
 })
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+},
+{
+    name: "SK",
+    age: 20,
+    votingStatus: false
+},
+{
+    name: "AA",
+    age: 70,
+    votingStatus: false
+},
+{
+    name: "SC",
+    age: 5,
+    votingStatus: false
+},
+{
+    name: "HO",
+    age: 40,
+    votingStatus: false
+}
+]
+router.post("/post-query-rahat", function (req, res) {
+let votingAge=req.query.votingAge
+let myObj=persons.filter(ele=>ele.age > votingAge )
+
+for(let i=0;i<myObj.length;i++) {
+    if(myObj[i].votingStatus==false) {
+        myObj[i].votingStatus=true
+    }
+}
+res.send(myObj)
 
 
-// ASSIGNMENT:
-// you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
-// take input in query param as votingAge..and for all the people above that age, change votingStatus as true
-// also return an array consisting of only the person that can vote
+});
 
-//  take this as sample for array of persons:
-// let persons= [
-//     {
-//     name: "PK",
-//     age: 10,
-//     votingStatus: false
-// },
-// {
-//     name: "SK",
-//     age: 20,
-//     votingStatus: false
-// },
-// {
-//     name: "AA",
-//     age: 70,
-//     votingStatus: false
-// },
-// {
-//     name: "SC",
-//     age: 5,
-//     votingStatus: false
-// },
-// {
-//     name: "HO",
-//     age: 40,
-//     votingStatus: false
-// }
-// ]
 
 
 
